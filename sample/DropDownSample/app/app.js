1c0dd8ca17beddc96a10d21af12b1263d21bbc38
Peter Staev <peter@tangrasoft.com>
2015-12-24 12:30:36 +0200

    changed project files to be able to be built with {N} CLI tools

---
+++ sample/DropDownSample/app/app.js
@@ -0,0 +1,5 @@
+var application = require("application");
+application.mainModule = "main-page";
+// Remove this in the AppBuilder templates
+application.cssFile = "./app.css";
+application.start();
