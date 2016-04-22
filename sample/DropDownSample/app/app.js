26b2a316d112f929c6e5151bec3c1ba753ba4336
Peter Staev <peter@tangrasoft.com>
2016-04-22 15:18:35 +0300

    migrate sample to vscode

--- sample/DropDownSample/app/app.js
+++
@@ -1,5 +0,0 @@
-var application = require("application");
-application.mainModule = "main-page";
-// Remove this in the AppBuilder templates
-application.cssFile = "./app.css";
-application.start();
