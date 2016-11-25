01c80334a67e9be8a23121c5e9f619065bc55067
Peter Staev <peter@tangrasoft.com>
2016-11-25 14:07:16 +0200

    upgrade sample to TS 2.0

--- sample/references.d.ts
+++ sample/references.d.ts
@@ -1,2 +1,12 @@
-/// <reference path="./node_modules/tns-core-modules/tns-core-modules.d.ts" /> Needed for autocompletion and compilation.
+/// <reference path="node_modules/typescript/lib/lib.es2015.core.d.ts" />
+/// <reference path="node_modules/typescript/lib/lib.es2015.generator.d.ts" />
+/// <reference path="node_modules/typescript/lib/lib.es2015.proxy.d.ts" />
+/// <reference path="node_modules/typescript/lib/lib.es2015.reflect.d.ts" />
+/// <reference path="node_modules/typescript/lib/lib.es5.d.ts" />
+
+/// <reference path="node_modules/tns-core-modules/tns-core-modules.d.ts" />
+/// <reference path="node_modules/tns-core-modules/lib.dom.d.ts" />
+
 /// <reference path="./node_modules/nativescript-drop-down/drop-down.d.ts" />
+/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
+/// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
