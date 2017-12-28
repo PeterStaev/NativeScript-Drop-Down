925391bd92fdb1a6fae52956036236ef767e29db
Peter Staev <peter@tangrasoft.com>
2017-12-28 19:20:38 +0200

    change build script

--- gruntfile.js
+++ gruntfile.js
@@ -67,7 +67,9 @@
             },
             "ci-webpack-demo": {
                 cmd: function (platform, demoSuffix) {
-                    return "cd demo" + (demoSuffix != "" ? "-" + demoSuffix : "")+ " && npm install && npm run ns-bundle --" + platform + " --build-app --uglify --snapshot";
+                    return "cd demo" + (demoSuffix != "" ? "-" + demoSuffix : "") + " && npm install && tns build " + platform
+                        + " --env.uglify --env.snapshot"
+                        + (demoSuffix === "ng" ? " --env.aot" : "");
                 }
             },
             npm_publish: {
