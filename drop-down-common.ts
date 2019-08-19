489a086cee74c1936833674f664bc1cc304c5054
AWhiteSheep <43411943+AWhiteSheep@users.noreply.github.com>
2019-08-18 20:44:30 -0400

    Update drop-down-common.ts

--- drop-down-common.ts
+++ drop-down-common.ts
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ***************************************************************************** */
 import { ObservableArray } from "data/observable-array";
-import { CSSType, CoercibleProperty, EventData, InheritedCssProperty, makeParser, makeValidator, Property, View } from "ui/core/view";
+import { CSSType, CoercibleProperty, EventData, Property, View, makeParser, makeValidator } from "ui/core/view";
 import { addWeakEventListener, removeWeakEventListener } from "ui/core/weak-event-listener";
 import { ItemsSource } from "ui/list-picker";
 import { TextAlignment } from "ui/text-base";
